44010af8e24ed1178dfbac3740e9b327397cebab
Erasings <78844597+Erasings@users.noreply.github.com>
2021-02-25 10:52:21 +0530

    hopefully a prestige fix
    prestige should be fixed

--- tests/games/bedwars/LevelTest.php
+++ tests/games/bedwars/LevelTest.php
@@ -3,12 +3,12 @@
 namespace Plancke\Tests\games\bedwars;
 
 use PHPUnit\Framework\TestCase;
-use Plancke\HypixelPHP\util\games\GameUtils;
+use Plancke\Hypixel-PHP\util\games\GameUtils;
 
 class LevelTest extends TestCase {
 
     function testLevels() {
-        $this->_testLevel(0, 0, 0);
+        $this->_testLevel(0, 1, 0);
 
         $this->_testLevel(708325, 146, 1);
         $this->_testLevel(199146, 42, 0);
@@ -24,6 +24,6 @@ class LevelTest extends TestCase {
         $level = GameUtils::getBedWars()->getExpCalculator()->getLevelForExp($exp);
         $this->assertEquals($expectedLevel, $level);
         $prestige = GameUtils::getBedWars()->getExpCalculator()->getPrestigeForLevel($level);
-        $this->assertEquals($expectedPrestigeOrdinal, $prestige->getOrdinal());
+        $this->assertEquals($expectedPrestigeOrdinal, $prestige->getOrdinal($prestige));
     }
 }
